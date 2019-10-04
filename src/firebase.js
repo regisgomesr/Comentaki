ce7b3d170dfa93a99d71bf20d47061e152241f81
Regis Ribeiro <regisgomesr@gmail.com>
2019-10-03 21:23:04 -0300

    Setando e configurando firebase / Limpando conexão com firebase.

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app'
+import 'firebase/database'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA8eVblEOSNrm1GuI_JgIWk01GOECplW8A",
+    authDomain: "comentaki-app-devpleno.firebaseapp.com",
+    databaseURL: "https://comentaki-app-devpleno.firebaseio.com",
+    projectId: "comentaki-app-devpleno",
+    storageBucket: "",
+    messagingSenderId: "811415908251",
+    appId: "1:811415908251:web:fd718b19c766c850a27b3e"
+  }
+  firebase.initializeApp(firebaseConfig)
+
+  export default firebase
