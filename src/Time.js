013ac9c383730924f3b8254a937d0fe5d0b339c0
Regis Ribeiro <regisgomesr@gmail.com>
2019-10-06 15:18:26 -0300

    Atualizando Time day para pegar o dia do mês, não só o dia da semana.

--- src/Time.js
+++ src/Time.js
@@ -4,7 +4,7 @@ const Time = ({ TIMESTAMP }) => {
     const hours = date.getHours()
     const minutes = '0'+date.getMinutes()
     const seconds = '0'+date.getSeconds()
-    const day = '0'+(date.getDay()-1)
+    const day = '0'+(date.getDate())
     const month = '0'+(date.getMonth()+1)
     const year = date.getFullYear()
   
