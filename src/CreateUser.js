390a3fdd741ba430a0e9b9566e55e65b37183dc3
Regis Ribeiro <regisgomesr@gmail.com>
2019-10-07 15:20:06 -0300

    Modificando createUser para o usuario criar uma conta pelo formulario.

--- src/CreateUser.js
+++ src/CreateUser.js
@@ -1,16 +1,35 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { AuthContext } from './auth'
 
 const CreateUser = () => {
     const auth = useContext(AuthContext)
+    const [form, setForm] = useState({ email: '', passwd: '' })
+
+    const onChange = campo => evt => {
+        setForm({
+            ...form,
+            [campo]: evt.target.value
+        })
+    }
+
+    if(auth.user !== null){
+        return null
+      }
+
     return (
         <>
             <br />
-            {JSON.stringify(auth.createUser)}
+            <h3>Criar conta:</h3>
+            {
+                auth.createUser.createUserState.error !== '' &&
+                <p>{ auth.createUser.createUserState.error }</p>
+            }
+            <input type='text' placeholder='Digite seu e-mail' value={form.email} onChange={onChange('email')} />
+            <input type='password' placeholder='Digite sua senha' value={form.passwd} onChange={onChange('passwd')} />
             <br />
             <button onClick={() => {
-                auth.createUser.createUser('regisgomesrr@gmail.com', 'abc123')
-            }}>Logar</button>
+                auth.createUser.createUser(form.email, form.passwd)
+            }}>Criar conta</button>
         </ >
     )
 }
